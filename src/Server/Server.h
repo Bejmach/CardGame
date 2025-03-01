29c58b592825eb3bca8aa60b07094d9f2a87eaa6
Bejmach <bejmach@gmail.com>
2025-03-01 16:11:53 +0100

    no server

--- src/Server/Server.h
+++ src/Server/Server.h
@@ -12,14 +12,6 @@ class Server{
 		std::vector<GameMaster* > games;
 
 		bool CreateGame();
-		bool AddPlayer(u_int16_t gameId, ClientBase* client);
-		bool PrepGame(u_int16_t gameId);
-		bool StartGame(u_int16_t gameId);
-
-		bool DrawCard(u_int16_t gameId, u_int8_t localPlayerId);
-		bool PlayCard(u_int16_t gameId, u_int8_t localPlayerId, u_int8_t card);
-
-		bool Command(u_int64_t command);
 
 	public:
 		Server();
